1525c44680cde5f9c3bf29a6071c92a273278537
Mithlesh Kumar Gupta <84225437+mithleshgupta@users.noreply.github.com>
2025-05-21 14:10:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/docs', docRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
